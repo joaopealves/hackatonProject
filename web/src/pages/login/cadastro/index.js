9e1ef815ffd233db620b6a30d3bc85d490c5f88a
João Pedro Alves <jotalmeida007@hotmail.com>
2020-10-31 16:53:37 -0300

    Add register page

--- web/src/pages/login/cadastro/index.js
+++ web/src/pages/login/cadastro/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { CgArrowLeft } from 'react-icons/cg';
+import { Link } from 'react-router-dom';
 
 export default function Register() {
   return (
     <div>
-      <p>Registro</p>
+      <div className="loginBackground">
+        <div className="loginContainer">
+          <div className="backIcon">
+            <Link to="/">
+              <CgArrowLeft color="white" size="58" />
+            </Link>
+          </div>
+          <p class="loginTitle">Cadastro</p>
+          <input type="text" placeholder="Nome Completo" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Senha" />
+          <input type="password" placeholder="Confirmar Senha" />
+          <input type="submit" value="CADASTRO" />
+          <div className="loginWith"></div>
+        </div>
+      </div>
     </div>
   );
 }
--- web/src/pages/login/login/index.js
+++ web/src/pages/login/login/index.js
@@ -4,11 +4,19 @@ import Google from '../../../assets/google.png';
 import Deezer from '../../../assets/deezer.png';
 import Facebook from '../../../assets/facebook.png';
 import Spotfy from '../../../assets/spotfy.png';
+import { CgArrowLeft } from 'react-icons/cg';
+import { Link } from 'react-router-dom';
 
 export default function Login() {
   return (
     <div className="loginBackground">
       <div className="loginContainer">
+        <div className="backIcon">
+          <Link to="/">
+            <CgArrowLeft color="white" size="58" />
+          </Link>
+        </div>
+        <p class="loginTitle">Login</p>
         <input type="email" placeholder="Email" />
         <input type="password" placeholder="Senha" />
         <input type="submit" value="LOGIN" />
@@ -29,6 +37,11 @@ export default function Login() {
         <a>
           <p>Esqueci minha senha</p>
         </a>
+        <Link to="/register">
+          <a>
+            <p>Não sou cadastrado</p>
+          </a>
+        </Link>
       </div>
     </div>
   );
