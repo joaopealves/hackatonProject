bdea81b5b72918e66f2883afffb782e5eabd57f6
João Pedro Alves <jotalmeida007@hotmail.com>
2020-10-31 14:48:53 -0300

    Add html components

--- web/src/pages/login/login/index.js
+++ web/src/pages/login/login/index.js
@@ -2,8 +2,16 @@ import React from 'react';
 
 export default function Login() {
   return (
-    <div>
-      <p>login</p>
+    <div className="">
+      <input type="text" placeholder="email" />
+      <input type="password" placeholder="email" />
+      <input type="submit" value="LOGIN" />
+      <input type="submit" value="logar com Github" />
+      <input type="submit" value="Logar com Google" />
+      <input type="submit" value="Logar com Facebook" />
+      <a>
+        <p>Esqueci minha senha</p>
+      </a>
     </div>
   );
 }
