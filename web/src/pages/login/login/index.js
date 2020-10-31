c65310e697f1ac0cf355355d54af06ca51854035
João Pedro Alves <jotalmeida007@hotmail.com>
2020-10-31 16:00:23 -0300

    add login page

--- web/src/pages/login/login/index.js
+++ web/src/pages/login/login/index.js
@@ -1,17 +1,35 @@
 import React from 'react';
+import './styles/styles.css';
+import Google from '../../../assets/google.png';
+import Deezer from '../../../assets/deezer.png';
+import Facebook from '../../../assets/facebook.png';
+import Spotfy from '../../../assets/spotfy.png';
 
 export default function Login() {
   return (
-    <div className="">
-      <input type="text" placeholder="email" />
-      <input type="password" placeholder="email" />
-      <input type="submit" value="LOGIN" />
-      <input type="submit" value="logar com Github" />
-      <input type="submit" value="Logar com Google" />
-      <input type="submit" value="Logar com Facebook" />
-      <a>
-        <p>Esqueci minha senha</p>
-      </a>
+    <div className="loginBackground">
+      <div className="loginContainer">
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Senha" />
+        <input type="submit" value="LOGIN" />
+        <div className="loginWith">
+          <button className="loginItem">
+            <img src={Google} />
+          </button>
+          <button className="loginItem">
+            <img className="Deezer" src={Deezer} />
+          </button>
+          <button className="loginItem">
+            <img className="Facebook" src={Facebook} />
+          </button>
+          <button className="loginItem">
+            <img className="Deezer" src={Spotfy} />
+          </button>
+        </div>
+        <a>
+          <p>Esqueci minha senha</p>
+        </a>
+      </div>
     </div>
   );
 }
