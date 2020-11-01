4d8213c1755fee29d6ca6a6ed6fbe970a5f1c8fd
João Pedro Alves <jotalmeida007@hotmail.com>
2020-10-31 22:14:14 -0300

    top button css

--- web/src/components/navbar/index.js
+++ web/src/components/navbar/index.js
@@ -2,39 +2,47 @@ import React from 'react';
 import './styles/styles.css';
 import { Link } from 'react-router-dom';
 import Routes from '../../routes';
-// import { FiChevronDown } from 'react-icons/vsc';
+import { FiChevronUp } from 'react-icons/fi';
 import { FiChevronDown } from 'react-icons/fi';
 export default function Navbar() {
   return (
-    <div className="navbar">
-      <div className="link">
-        <a href="#HowWork" className="linkItem">
-          Como funciona
-        </a>
-        <a href="#AboutUs" className="linkItem">
-          Quem somos
-        </a>
+    <div>
+      <div id="navbar" className="navbar">
+        <div className="logoContainer">Hydra</div>
+        <div className="link">
+          <a href="#HowWork" className="linkItem">
+            Como funciona
+          </a>
+          <a href="#AboutUs" className="linkItem">
+            Quem somos
+          </a>
 
-        <div className="dropdown linkItem">
-          <button className=" dropbtn">
-            Vantagens
-            <FiChevronDown size="12px" />
-            <i className="fa fa-caret-down"></i>
-          </button>
-          <div className="dropdown-content">
-            <a href="#Listeners">Ouvintes</a>
-            <a href="#Compaines">Empresas</a>
-            <a href="#Artists">Artistas</a>
+          <div className="dropdown linkItem">
+            <button className=" dropbtn">
+              Vantagens
+              <FiChevronDown size="12px" />
+              <i className="fa fa-caret-down"></i>
+            </button>
+            <div className="dropdown-content">
+              <a href="#Listeners">Ouvintes</a>
+              <a href="#Compaines">Empresas</a>
+              <a href="#Artists">Artistas</a>
+            </div>
           </div>
+          <a className="linkItem">Parceiros</a>
         </div>
-        <a className="linkItem">Parceiros</a>
+        <Link to="/login">
+          <button className="button buttonLogin">Login</button>
+        </Link>
+        <Link to="/register">
+          <button className="button buttonRegister">Cadastro</button>
+        </Link>
       </div>
-      <Link to="/login">
-        <button className="button buttonLogin">Login</button>
-      </Link>
-      <Link to="/register">
-        <button className="button buttonRegister">Cadastro</button>
-      </Link>
+      <div className="topButton">
+        <a href="#navbar" id="topButton">
+          <FiChevronUp color="white" size="50px" />
+        </a>
+      </div>
     </div>
   );
 }
